3f8c48210d082351e099d17a1870401426395ef9
pedroHenrique <pedro.pdhps@gmail.com>
2026-09-13 14:54:52 -0300

    ajustes no banner

--- javascript/banner.js
+++ javascript/banner.js
@@ -2,7 +2,9 @@ let video = document.getElementById("documentario");
 let banner = document.getElementsByClassName("banner");
 let colaboradores = document.getElementsByClassName("grid-colaboradores");
 let nomeDocumentario =document.getElementById("nome-documentario");
+let btnRetornar = document.getElementById("btn-retornar");
 
+btnRetornar.style.display = "none";
 video.style.display = "none";
 
 function ShowVideo() {
@@ -12,17 +14,18 @@ function ShowVideo() {
         banner[0].style.display = "none";
         colaboradores[0].style.display = "none";
         nomeDocumentario.style.display = "none";
-
+        
+        btnRetornar.style.display = "block";
+        document.getElementById("btn-show").style.display = "none";
         video.style.display = "block";
-        btn.innerHTML = "&#10146;&#65039;";
-        btn.style.opacity = 0.5
     }else{
         banner[0].style.display = "block";
         colaboradores[0].style.display = "flex";
         nomeDocumentario.style.display = "block";
 
+        btnRetornar.style.display = "none";
+        document.getElementById("btn-show").style.display = "block";
+        
         video.style.display = "none";
-        btn.innerHTML = "&#10146;&#65039;";
-        btn.style.opacity = 1
     }
 }
