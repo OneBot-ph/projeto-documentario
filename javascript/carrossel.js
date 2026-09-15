2c802b99c053f18957a033b9fc1e6986bcbc08ab
pedroHenrique <pedro.pdhps@gmail.com>
2026-09-15 17:10:13 -0300

    carrossel

--- javascript/carrossel.js
+++ javascript/carrossel.js
@@ -1,14 +1,37 @@
-let indice = 1;
-let imgs = document.getElementsByClassName("imgs");
-let img = document.querySelectorAll("#imgs img");
+let slidesImg = document.getElementsByClassName("slide-img");
+let atalhos = document.getElementsByClassName("atalhos-img")
 
-function ShowImg(){
+let indiceSlide = 0;
 
-    if(indice > imgs.length)
-    {
-        indice = 0;
+CarrosselGaleria(indiceSlide);
+
+function ProximoSlide(valor){
+    CarrosselGaleria(indiceSlide += valor)
+}
+
+function Atalho(valor){
+    CarrosselGaleria(indiceSlide = valor)
+
+}
+
+function CarrosselGaleria(valor){
+
+    if(valor > slidesImg.length){
+        indiceSlide = 1;
     }
 
-    imgs.style.transform = translatex(${-img * 400}px);
+    if(valor < 1){
+        indiceSlide = slidesImg.length
+    }
 
-} 
+    for(let i = 0; i < atalhos.length; i++){
+        atalhos[i].className = atalhos[i].className.replace(" ativo", "")
+    }
+
+    for(let i = 0; i < slidesImg.length; i++){
+        slidesImg[i].style.display = "none";
+    }
+
+    slidesImg[indiceSlide - 1].style.display = "block"
+    atalhos[indiceSlide - 1].className += " ativo";
+}
